037364e964e898e5cc062df97c476b001d131efe
EnumaOub <rayane.oubenali@gmail.com>
2024-07-14 11:57:45 +0200

    Can delete value in tree

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -70,19 +70,58 @@ class Tree {
     };
 
     deleteItem(value) {
-
+        return this.deleteNode(value, this.root)
     };
+
+    deleteNode(value, node) {
+        if (!node) {
+            return node;
+        }
+        if (value < node.data) {
+            node.left = this.deleteNode(value, node.left);
+        }
+        else if (value > node.data) {
+            node.right = this.deleteNode(value, node.right);
+        }
+        else if (value === node.data) {
+            if (node.left === null) {
+                return node.right;
+            }
+            else if (node.right === null) {
+                return node.left;
+            }
+            else {
+                node.data = this.minValue(node.right);
+                node.right = this.deleteNode(node.data, node.right);
+            }
+        }
+        return node;
+    };
+
+    minValue(node) {
+        let minValue = node.data;
+        while (node.letf) {
+            minValue = node.left.data;
+            node = node.left;
+        }
+        return minValue;
+    }
 };
 
 
 const BST = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
+console.log("Initial DATA : \n")
 BST.showNode(BST.root)
+
 BST.insert(25);
+console.log("\nInsert 25 DATA : \n")
 BST.showNode(BST.root)
+
 BST.insert(5555);
+console.log("\nInsert 5555 DATA : \n")
 BST.showNode(BST.root)
-BST.insert(2);
-BST.showNode(BST.root)
-BST.insert(8888);
+
+BST.deleteItem(23);
+console.log("\nDelete 23 DATA : \n")
 BST.showNode(BST.root)
